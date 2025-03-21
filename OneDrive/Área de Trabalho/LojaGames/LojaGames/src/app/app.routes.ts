8457d15d8b776e7a325c2a4a528636d5f9719504
Gabriel <160122283+gabsantosmr@users.noreply.github.com>
2025-03-21 14:52:26 -0300

    Delete OneDrive/Área de Trabalho/LojaGames/LojaGames directory

--- OneDrive/Área de Trabalho/LojaGames/LojaGames/src/app/app.routes.ts
+++
@@ -1,9 +0,0 @@
-import { Routes } from '@angular/router';
-import { InicioComponent } from './inicio/inicio.component';
-import { LoginComponent } from './login/login.component';
-
-export const routes: Routes = [
-    {path:'inicio', component: InicioComponent},
-    {path:'login', component: LoginComponent},
-    {path:'', redirectTo:'/inicio',pathMatch:'full'}
-];
